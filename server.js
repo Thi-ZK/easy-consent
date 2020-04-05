c245b86fe4147ca6505ef5e91b3d938ae53c6aae
tensai1 <thi.yancha@gmail.com>
2020-04-05 22:51:40 +0200

    fixing parse error with fb key - try 1

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ admin.initializeApp({
 	  "type": process.env.FB_TYPE,
 	  "project_id": process.env.FB_PROJECT_ID,
 	  "private_key_id": process.env.FB_PK_ID,
-	  "private_key": process.env.FB_PK,
+	  "private_key": process.env.FB_PK.replace(/\\n/g, '\n'),
 	  "client_email": process.env.FB_CLIENT_EMAIL,
 	  "client_id": process.env.FB_CLIENT_ID,
 	  "auth_uri": process.env.FB_AUTH_URI,
