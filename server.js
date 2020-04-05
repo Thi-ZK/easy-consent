93e113f619f7179c9645d6988a16df082f979474
tensai1 <thi.yancha@gmail.com>
2020-04-05 22:07:12 +0200

    testing heroku config vars with fb

--- server.js
+++ server.js
@@ -12,16 +12,16 @@ app.use(express.static('public'));
 
 admin.initializeApp({
 	credential: admin.credential.cert({
-		"type": process.env.FB_TYPE,
-		"project_id": process.env.FB_PROJECT_ID,
-		"private_key_id": process.env.FB_PK_ID,
-		"private_key": process.env.FB_PK,
-		"client_email": process.env.FB_CLIENT_EMAIL,
-		"client_id": process.env.FB_CLIENT_ID,
-		"auth_uri": process.env.FB_AUTH_URI,
-		"token_uri": process.env.FB_TOKEN_URI,
-		"auth_provider_x509_cert_url": process.env.FB_AUTH_CERT_URL,
-		"client_x509_cert_url": process.env.FB_CLIENT_CERT_URL
+	  "type": process.env.FB_TYPE,
+	  "project_id": process.env.FB_PROJECT_ID,
+	  "private_key_id": process.env.FB_PK_ID,
+	  "private_key": process.env.FB_PK,
+	  "client_email": process.env.FB_CLIENT_EMAIL,
+	  "client_id": process.env.FB_CLIENT_ID,
+	  "auth_uri": process.env.FB_AUTH_URI,
+	  "token_uri": process.env.FB_TOKEN_URI,
+	  "auth_provider_x509_cert_url": process.env.FB_AUTH_CERT_URL,
+	  "client_x509_cert_url": process.env.FB_CLIENT_CERT_URL
 	}),
 	databaseURL: "https://easy-consent.firebaseio.com"
 });
