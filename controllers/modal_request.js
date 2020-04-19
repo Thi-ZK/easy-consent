3593dd4a6039e07fd1d112acb4dc80f08cc819f2
tensai1 <thi.yancha@gmail.com>
2020-04-19 16:14:53 +0200

    adding modal_without_scan modals

--- controllers/modal_request.js
+++ controllers/modal_request.js
@@ -4,10 +4,6 @@ const path = require('path');
 const db_model = require("../models/modal_request.js");
 const middws = require("../middlewares/modal_request.js");
 
-router.get('/', (req, res) => {
-	res.send("E A TERRA TODA TREMER QUE EU JAMAIS VOU DEIXAR DE TE AMAR TE QUERER");
-});
-
 // For Production Handling  ---- Complete Modal With Cookie Scanning
 router.get('/modal', middws.cookie_verification_rdr, (req, res) => { // later on identify which type user wants the modal, with or without cookies descrp and if only warn modal
 	let domain = req.query.domain || "127.0.0.1:9999";
@@ -29,6 +25,33 @@ router.get('/modal', middws.cookie_verification_rdr, (req, res) => { // later on
 	});
 });
 
+//For Production Handling ---- Complete Modal Without Cookie Scanning
+router.get('/modal_ns', middws.cookie_verification_rdr, (req, res) => {
+	let domain = req.query.domain || "127.0.0.1:9999";
+	let lang = req.query.lang;
+	const modal_to_render = 'modals_without_cookie_scan/modal_js_' + (lang || 'br');
+
+	db_model.does_doc_exists('clients', domain).then(status => {
+		res.render(modal_to_render, {modal_domain_flag: domain});
+	}).catch(error => {
+		res.send("Error / E1");
+	});
+});
+
+// sets cookie to prevent readings from db, returning nothing since user don't need the modal
+router.get('/modal_closed', middws.cookie_set_rdr, (req, res) => {
+	res.send("");
+});
+
+// deletes cookie to enable readings from db again, user may have expired the config cookie
+router.get('/modal_restore', middws.cookie_clear_rdr, (req, res) => {
+	res.send("");
+});
+
+
+
+
+
 // For Implementation & Testing ---- Complete Modal With Cookie Scanning
 router.get('/TEST_MODAL', middws.check_if_host_is_local_host, (req, res) => {
 	db_model.get_sub_collection_all_docs('clients', 'zkitens.herokuapp', 'cookie_scan_results').then(all_docs => {
@@ -42,16 +65,6 @@ router.get('/TEST_MODAL', middws.check_if_host_is_local_host, (req, res) => {
 		// console.log("hallo");
 		res.send(error);
 	});
-});
-
-// sets cookie to prevent readings from db, returning nothing since user don't need the modal
-router.get('/modal_closed', middws.cookie_set_rdr, (req, res) => {
-	res.send("");
-});
-
-// deletes cookie to enable readings from db again, user may have expired the config cookie
-router.get('/modal_restore', middws.cookie_clear_rdr, (req, res) => {
-	res.send("");
 });
 
 module.exports = router;
