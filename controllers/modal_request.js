477d317073cf86a6dc6cae7dbfc91127745418f8
tensai1 <thi.yancha@gmail.com>
2020-04-26 16:51:01 +0200

    implementing warn modal

--- controllers/modal_request.js
+++ controllers/modal_request.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const db_model = require("../models/modal_request.js");
 const middws = require("../middlewares/modal_request.js");
 
 // For Production Handling  ---- Complete Modal With Cookie Scanning
@@ -16,6 +15,14 @@ router.get('/modal', middws.cookie_verification_rdr, middws.get_scanned_cookies_
 router.get('/modal_ns', middws.cookie_verification_rdr, middws.check_if_domain_is_registered, (req, res) => {
 	let lang = req.query.lang;
 	const modal_to_render = 'modals_without_cookie_scan/modal_js_' + (lang || 'br');
+
+	res.render(modal_to_render, res.locals.to_be_passed_data);
+});
+
+//For Production Handling ---- Complete Modal Only Warn
+router.get('/modal_warn', middws.cookie_verification_rdr, middws.check_if_domain_is_registered, (req, res) => {
+	let lang = req.query.lang;
+	const modal_to_render = 'modals_warn/modal_js_' + (lang || 'br');
 
 	res.render(modal_to_render, res.locals.to_be_passed_data);
 });
@@ -36,16 +43,6 @@ router.get('/modal_restore', middws.cookie_clear_rdr, (req, res) => {
 
 // For Implementation & Testing ---- Complete Modal With Cookie Scanning
 router.get('/TEST_MODAL', middws.check_if_host_is_local_host, (req, res) => {
-	// db_model.get_sub_collection_all_docs('clients', 'zkitens.herokuapp', 'cookie_scan_results').then(all_docs => {
-	// 	let all_data = {
-	// 		scan_data: all_docs,
-	// 	};
-
-	// 	res.render('modals_with_cookie_scan_TEST/modal_' + (req.query.lang || 'br'), all_data);
-	// }).catch(error => { console.log(error);
-	// 	res.send(error);
-	// });
-
 	res.render('modals_warn_TEST/modal_' + (req.query.lang || 'br'));
 });
 
